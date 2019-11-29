d9d7aea8f66028baa06d60ad9e30dd8c6b6c650d
Hector Salinas <hector_sag@outlook.com>
2019-11-29 09:24:46 -0600

    Initial commit

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'https://hsduiii-react-chat.herokuapp.com/';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, (error) => {
+     if(error) {
+       alert(error);
+     }
+    });
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    }
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    })
+
+    return () => {
+         socket.emit('disconnect');
+
+         socket.off();
+       }
+  }, [messages])
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={ room }/>
+        <Messages messages={ messages } name={ name }/>
+        <Input message={ message } sendMessage={ sendMessage } setMessage={ setMessage }/>
+      </div>
+      <TextContainer users={ users } />
+    </div>
+  )
+};
+
+export default Chat;
